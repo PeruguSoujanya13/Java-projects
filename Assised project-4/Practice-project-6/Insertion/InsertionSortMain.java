ba8a99cbb9122397c7192c68348d1249d67f7cda
Perugu Soujanya <122893597+PeruguSoujanya13@users.noreply.github.com>
2023-05-20 03:35:57 -0700

    Add files via upload

---
+++ Assised project-4/Practice-project-6/Insertion/InsertionSortMain.java
@@ -0,0 +1,34 @@
+package Insertion;
+
+import java.util.Arrays;
+
+public class InsertionSortMain {
+	
+	public static void main(String[] args) {
+		int[] arr = { 9, 6, 77, 1, 2, 46, 5 };
+
+		System.out.println("GIVEN ARRAY : " + Arrays.toString(arr));
+
+		insertionSort(arr);
+
+		System.out.println("SORTED ARRAY : " + Arrays.toString(arr));
+
+	}
+
+	private static void insertionSort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) {
+
+			int temp = arr[i];
+			int j = i - 1;
+
+			while (j>-1 && temp < arr[j]) {
+				// swap 
+				arr[j+1]=arr[j];
+				arr[j] = temp;
+				j--;
+			}
+
+		}
+	}
+}
